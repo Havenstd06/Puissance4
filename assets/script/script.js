b9e657f8356a3b184964edf8fdf8253dcf3e8226
TavaresDylan <tavares-dylan03@gmail.com>
2019-05-07 14:05:42 +0200

    JS2Fin

--- assets/script/script.js
+++ assets/script/script.js
@@ -20,7 +20,7 @@ function newGame(){
 	creerTableau();
 }
 function afficheTextAnnonce(text){
-	document.GetElementById('textannonce').innerHTML;
+	document.getElementById('textannonce').innerHTML;
 }
 function nomDuJoueur(numerojoueur){
 	if (numerojoueur == 1) {
@@ -28,4 +28,26 @@ function nomDuJoueur(numerojoueur){
 	}else{
 		return "bleu";
 	}
+}
+function creerTableau(){
+	text ="<table>".innerHTML;
+	for (var i = 0; i < lignes.length; i++) {
+		text = "<tr>".innerHTML;
+		for (var j = 0; j < colonnes.length; j++) {
+			text = "<td id='[i]-[j]' onclick='detectClique([j])'>".innerHTML;
+			if (plateau[i][j] == 1) {
+				text = "<div class='joueur1'></div>".innerHTLM;
+			}else if (plateau[i][j] == 2){
+				text = "<div class='joueur2'></div>".innerHTLM;
+			}else{
+
+			}
+		text = "</td>".innerHTML;
+
+		}
+	text = "</tr>".innerHTLM;
+	}
+text = "</table>";
+text = document.getElementById('puissance4');
+
 }
