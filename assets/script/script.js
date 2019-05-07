871253fa78ab8325a913d0fbf2781a010db11440
TavaresDylan <tavares-dylan03@gmail.com>
2019-05-07 11:55:52 +0200

    JS1

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,3 +1,15 @@
+var numerojoueur = 1;
+var colonnes = 5;
+var lignes = 5;
+var jeu = true;
+var text = "";
+var plateau = [];
+
+for (var i = 0; i < lignes.length; i++) {
+	var plateau[i];
+}
+
+
 function newGame(){
 	
 }
